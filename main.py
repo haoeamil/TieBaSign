95326099ff87e853e36c95498dfce6441b7c54c3
haoeamil <102909465+haoeamil@users.noreply.github.com>
2023-01-13 12:49:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# -*- coding:utf-8 -*-
-import os
-import requests
-import hashlib
-import time
-import copy
-import logging
-import random
-
-import smtplib
-
-    logger.info("所有用户签到结束")
+print("123");
 
 
